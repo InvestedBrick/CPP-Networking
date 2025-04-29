18535506b10fee7029f27f11a77499c074a11b04
InvestedBrick <julianbrecht25@gmail.com>
2025-04-29 20:31:18 +0200

    Made server multithreaded

--- server/main.cpp
+++ server/main.cpp
@@ -5,17 +5,98 @@
 #include <string>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <thread>
+#include <mutex>
+#include <queue>
+
+std::mutex mtx;
 
 #define MAX_BUF_SIZE 4096
 #define PORT 8080
 #define FAILED -1
+
+
 void log(const std::string &message) {
+    std::lock_guard<std::mutex> lock(mtx);
     std::cout << "[LOG] " << message << std::endl;
 }
 
 void fail(const std::string &message) {
     std::cerr << "[FAIL] " << message << std::endl;
     exit(EXIT_FAILURE);
+}
+
+void warn(const std::string &message) {
+    std::lock_guard<std::mutex> lock(mtx);
+    std::cerr << "[WARN] " << message << std::endl;
+}
+
+void handle_client(void* args){
+    int client_socket = *(int*)args;
+    // recieve messages
+
+    char buf[MAX_BUF_SIZE];
+    while(true){
+        memset(buf,0, MAX_BUF_SIZE); // clear buffer
+        int bytes_recieved = recv(client_socket, buf, MAX_BUF_SIZE, 0);
+        if (bytes_recieved == FAILED){
+            warn("Failed to receive message");
+        }else if (bytes_recieved == 0){
+            log("Client disconnected");
+            break;
+        }
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            std::cout << "Recieved: " << std::string(buf,0,bytes_recieved) << std::endl;
+        }
+        send(client_socket, buf, bytes_recieved + 1, 0); // echo back
+    } 
+  
+    // Close socket
+    close(client_socket);
+    
+}
+
+void listener_thread(void* args){
+    int socketfd = *(int*)args;
+    while(true){
+
+        sockaddr_in client;
+        socklen_t client_size = sizeof(client);
+        char host[NI_MAXHOST];
+        char service[NI_MAXSERV];
+        
+        int client_socket = accept(socketfd, (sockaddr*) &client, &client_size);
+        
+        if (client_socket == FAILED){
+            fail("Failed to accept connection");
+        }else{
+            log("Client connected");
+        }
+
+        memset(host, 0, NI_MAXHOST); // clear out the host string
+        memset(service, 0, NI_MAXSERV); // clear out the service string
+
+        int result = getnameinfo((sockaddr*) & client, client_size, host,NI_MAXHOST, service, NI_MAXSERV, 0);
+
+        if (result) {
+            {
+                std::lock_guard<std::mutex> lock(mtx);
+                std::cout << host << " connected on " << service << std::endl;
+            }
+        }else{
+            inet_ntop(AF_INET, &client.sin_addr,host, NI_MAXHOST);
+            {
+                std::lock_guard<std::mutex> lock(mtx);
+                std::cout << host << " connected on " << ntohs(client.sin_port) << std::endl;
+            }
+        }
+
+        // create a thread to handle the client
+        std::thread client_handler(handle_client,&client_socket);
+        client_handler.detach();
+    }
+    
 }
 
 int main() {
@@ -49,54 +130,11 @@ int main() {
     }else{
         log("Listening on port " + std::to_string(PORT));
     }
-    // accept connections
 
-    sockaddr_in client;
-    socklen_t client_size = sizeof(client);
-    char host[NI_MAXHOST];
-    char service[NI_MAXSERV];
+    // create a listening thread
+    std::thread listener(listener_thread, &socketfd);
+    listener.join();
 
-    int client_socket = accept(socketfd, (sockaddr*) &client, &client_size);
-
-    if (client_socket == FAILED){
-        fail("Failed to accept connection");
-    }else{
-        log("Client connected");
-    }
-
-    close(socketfd); // close the listening socket
-
-    memset(host, 0, NI_MAXHOST); // clear out the host string
-    memset(service, 0, NI_MAXSERV); // clear out the service string
-
-    int result = getnameinfo((sockaddr*) & client, client_size, host,NI_MAXHOST, service, NI_MAXSERV, 0);
-
-    if (result) {
-        std::cout << host << " connected on " << service << std::endl;
-    }else{
-        inet_ntop(AF_INET, &client.sin_addr,host, NI_MAXHOST);
-        std::cout << host << " connected on " << ntohs(client.sin_port) << std::endl;
-    }
-    // recieve messages
-
-    char buf[MAX_BUF_SIZE];
-
-    while(true){
-        memset(buf,0, MAX_BUF_SIZE); // clear buffer
-        int bytes_recieved = recv(client_socket, buf, MAX_BUF_SIZE, 0);
-        if (bytes_recieved == FAILED){
-            fail("Failed to receive message");
-        }else if (bytes_recieved == 0){
-            log("Client disconnected");
-            break;
-        }
-
-        std::cout << "Recieved: " << std::string(buf,0,bytes_recieved) << std::endl;
-
-        send(client_socket, buf, bytes_recieved + 1, 0); // echo back
-    } 
-    // Close socket
-    close(client_socket);
-    log("Socket closed");
+    close(socketfd);
     return 0;
 }
