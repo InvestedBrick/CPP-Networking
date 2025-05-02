82eca62951a3e583c73c6f600a7877dec695015d
InvestedBrick <julianbrecht25@gmail.com>
2025-05-02 23:42:43 +0200

    Added curso for current position

--- shared/colors.hpp
+++ shared/colors.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <string>
 
 #define ANSI_COLOR_BLACK    30
@@ -10,33 +11,36 @@
 #define ANSI_COLOR_WHITE    37
 #define ANSI_COLOR_RESET    0
 
+#define ANSI_COLOR_BACKGROUND_WHITE 47
 #define PIXEL_CHAR "#"
 
 std::string color_to_string(int color) {
     switch (color) {
-        case ANSI_COLOR_BLACK:   return "\033[1;30m";
-        case ANSI_COLOR_RED:     return "\033[1;31m";
-        case ANSI_COLOR_GREEN:   return "\033[1;32m";
-        case ANSI_COLOR_YELLOW:  return "\033[1;33m";
-        case ANSI_COLOR_BLUE:    return "\033[1;34m";
-        case ANSI_COLOR_MAGENTA: return "\033[1;35m";
-        case ANSI_COLOR_CYAN:    return "\033[1;36m";
-        case ANSI_COLOR_WHITE:   return "\033[1;37m";
-        default:                 return "\033[0m"; 
+        case ANSI_COLOR_BLACK:            return "\033[1;30m";
+        case ANSI_COLOR_RED:              return "\033[1;31m";
+        case ANSI_COLOR_GREEN:            return "\033[1;32m";
+        case ANSI_COLOR_YELLOW:           return "\033[1;33m";
+        case ANSI_COLOR_BLUE:             return "\033[1;34m";
+        case ANSI_COLOR_MAGENTA:          return "\033[1;35m";
+        case ANSI_COLOR_CYAN:             return "\033[1;36m";
+        case ANSI_COLOR_WHITE:            return "\033[1;37m";
+        case ANSI_COLOR_BACKGROUND_WHITE: return "\033[1;47m";
+        default:                          return "\033[0m"; 
     }
 }
 
 char color_to_char(int color) {
     switch (color) {
-        case ANSI_COLOR_BLACK:   return 'B';
-        case ANSI_COLOR_RED:     return 'R';
-        case ANSI_COLOR_GREEN:   return 'G';
-        case ANSI_COLOR_YELLOW:  return 'Y';
-        case ANSI_COLOR_BLUE:    return 'U';
-        case ANSI_COLOR_MAGENTA: return 'M';
-        case ANSI_COLOR_CYAN:    return 'C';
-        case ANSI_COLOR_WHITE:   return 'W';
-        default:                 return ' '; 
+        case ANSI_COLOR_BLACK:            return 'B';
+        case ANSI_COLOR_RED:              return 'R';
+        case ANSI_COLOR_GREEN:            return 'G';
+        case ANSI_COLOR_YELLOW:           return 'Y';
+        case ANSI_COLOR_BLUE:             return 'U';
+        case ANSI_COLOR_MAGENTA:          return 'M';
+        case ANSI_COLOR_CYAN:             return 'C';
+        case ANSI_COLOR_WHITE:            return 'W';
+        case ANSI_COLOR_BACKGROUND_WHITE: return 'X';
+        default:                          return ' '; 
     }
 }
 
@@ -50,6 +54,7 @@ int char_to_color(char c){
         case 'M':   return ANSI_COLOR_MAGENTA;
         case 'C':   return ANSI_COLOR_CYAN;
         case 'W':   return ANSI_COLOR_WHITE;
+        case 'X':   return ANSI_COLOR_BACKGROUND_WHITE;
         default:    return ANSI_COLOR_RESET;
     }
 
