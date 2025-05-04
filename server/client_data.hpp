ba33bdc16e083d89111925701d758ba774e9bcf6
InvestedBrick <julianbrecht25@gmail.com>
2025-05-04 20:33:35 +0200

    Allow for color parsing

--- server/client_data.hpp
+++ server/client_data.hpp
@@ -62,63 +62,43 @@ public:
                 color_current_cell = true;
                 break;
             }
+            case '0':{
+                current_color = ANSI_COLOR_BLACK;
+                break;
+            }
+            case '1': {
+                current_color = ANSI_COLOR_WHITE;
+                break;
+            }
+            case '2': {
+                current_color = ANSI_COLOR_RED;
+                break;
+            }
+            case '3': {
+                current_color = ANSI_COLOR_GREEN;
+                break;
+            }
+            case '4': {
+                current_color = ANSI_COLOR_YELLOW;
+                break;
+            }
+            case '5': {
+                current_color = ANSI_COLOR_BLUE;
+                break;
+            }
+            case '6': {
+                current_color = ANSI_COLOR_MAGENTA;
+                break;
+            }
+            case '7': {
+                current_color = ANSI_COLOR_CYAN;
+                break;
+            }
             default:{
                 break;
             }
         }
     }
 
-    /*
-    void parse_cmd(const char* str,int n_bytes){
-        std::string data(str,0,n_bytes);
-        state_pos_x = pos_x;
-        state_pos_y = pos_y;
-
-        for (int idx = 0; idx < n_bytes;++idx){
-            switch(data[idx]){
-                case 'w': {
-                    if (pos_y == 0){
-                        pos_y = GRID_HEIGHT - 1;
-                    }else{
-                        pos_y--;
-                    }
-                    break;
-                }
-                case 'a':{
-                    if (pos_x == 0){
-                        pos_x = GRID_WIDTH - 1;
-                    }else{
-                        pos_x--;
-                    }
-                    break;
-                }
-                case 's': {
-                    if (pos_y == GRID_HEIGHT - 1){
-                        pos_y = 0;
-                    }else{
-                        pos_y++;
-                    }
-                    break;
-                }
-                case 'd': {
-                    if (pos_x == GRID_WIDTH - 1){
-                        pos_x = 0;
-                    }else{
-                        pos_x++;
-                    }
-                    break;
-                }
-                case 'c': {
-                    to_be_colored.push_back({pos_x,pos_y});
-                    break;
-                }
-                default:{
-                    return;
-                }
-            }
-            
-        }
-
-    }
-        */
+    
 };
