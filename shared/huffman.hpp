59bce91303fbb3fc7612d3343c533bdb6ec9a648
InvestedBrick <julianbrecht25@gmail.com>
2025-05-07 22:04:45 +0200

    Work in progress

---
+++ shared/huffman.hpp
@@ -0,0 +1,80 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <memory>
+
+class Huffman {
+private:
+
+    struct Node{
+        Node(){}
+        Node(uint weight_, std::unique_ptr<Node> left_, std::unique_ptr<Node> right_, char c_)
+        : weight(weight_), left(std::move(left_)), right(std::move(right_)), c(c_) {}
+        uint weight;
+        std::unique_ptr<Node> left = nullptr;
+        std::unique_ptr<Node> right = nullptr;
+        char c;
+    };
+
+    std::string& data;
+    std::vector<std::unique_ptr<Node>> freqs {};
+    std::vector<uint8_t> bytes {};
+
+    void set_bit_n(uint8_t& byte, uint8_t n){
+        byte |= (0x1 << n);
+    }
+    void calculate_frequencies(){
+        for (char c : data){
+            //Maybe vec of Nodes
+            auto find = std::find_if(freqs.begin(), freqs.end(), [&] (const std::unique_ptr<Node>& pair) {
+                return pair->c == c;
+            }); 
+            //char is not in set
+            if (find == freqs.end()){
+                freqs.push_back(std::make_unique<Node>(1,nullptr,nullptr,c));
+            }else{
+                (*find)->weight++;
+            }
+        }
+    }
+
+
+    bool cmp(const std::unique_ptr<Node>& a, const std::unique_ptr<Node>& b){
+        return a->weight > b->weight;
+    }
+    //sort the freqs in reverse order
+    void sort_frequencies(){
+        std::sort(freqs.begin(),freqs.end(),cmp);
+    }
+
+
+
+public:
+    Huffman(std::string& data_) : data(data_) {}
+
+    void encode(){
+        this->calculate_frequencies();
+        this->sort_frequencies();
+        
+        //build tree
+        while(freqs.size() > 1){
+            auto left = std::move(freqs.back()); freqs.pop_back();
+            auto right = std::move(freqs.back()); freqs.pop_back();
+            auto parent = std::make_unique<Node>();
+            parent->weight = left->weight + right->weight;
+            parent->left  = std::move(left);
+            parent->right = std::move(right);
+
+            freqs.push_back(std::move(parent));
+
+            this->sort_frequencies();
+
+        }
+
+        //encode string
+
+        
+    }
+
+};
